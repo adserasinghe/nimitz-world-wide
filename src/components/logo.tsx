00c080cee34b1a3d55c18e3c07dea1e0dbfabea9
Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2025-08-11 10:57:55 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Mountain classNam

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,11 @@
-import { Mountain } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
 export function Logo({className}: {className?: string}) {
   return (
     <Link href="/" className={cn("flex items-center gap-2", className)} prefetch={false}>
-      <Mountain className="h-6 w-6 text-primary" />
+      <Image src="https://i.postimg.cc/vHgV3cvx/471153132-122109371366653918-3126876278288396208-n.jpg" alt="Nimitz World Wide Logo" width={32} height={32} className="h-8 w-8 rounded-full" />
       <span className="text-lg font-semibold font-headline">Nimitz World Wide</span>
     </Link>
   );
