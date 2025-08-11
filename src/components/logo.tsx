b39705e3481abc0352e3c3dc2fae40ea1486cfe0
Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2025-08-11 11:00:39 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 export function Logo({className}: {className?: string}) {
   return (
     <Link href="/" className={cn("flex items-center gap-2", className)} prefetch={false}>
-      <Image src="https://i.postimg.cc/vHgV3cvx/471153132-122109371366653918-3126876278288396208-n.jpg" alt="Nimitz World Wide Logo" width={32} height={32} className="h-8 w-8 rounded-full" />
+      <Image src="https://i.postimg.cc/cCZdYkTc/471153132-122109371366653918-3126876278288396208-n-removebg-preview.png" alt="Nimitz World Wide Logo" width={32} height={32} className="h-8 w-8" />
       <span className="text-lg font-semibold font-headline">Nimitz World Wide</span>
     </Link>
   );
