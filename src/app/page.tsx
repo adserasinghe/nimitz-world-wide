933fdaed2c3398c805b045e9fee8d3939a14c514
Aditha Dinuja Serasinghe <svadithadinujaserasinghe@gmail.com>
2025-08-12 05:12:46 +0000

    fix the fade in is not working on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="animate-in fade-in-50 duration-500">
+    <div>
       <Hero />
       <Services />
       <Portfolio />
